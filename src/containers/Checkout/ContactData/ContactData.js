aff73ca0ca14fecc7d6637bc8ebcdc833c5cf413
mutasem <motasemimad10@gmail.com>
2021-01-17 15:51:51 +0200

    learn react burger-app module13 lec5

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,13 +8,59 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
-        loading: false
+        orderForm: {
+                name: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your Name'
+                    },
+                    value: '',
+                },
+                street: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Street'
+                    },
+                    value: '',
+                },
+                zipCode: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'ZIP Code'
+                    },
+                    value: '',
+                },
+                country: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Country'
+                    },
+                    value: '',
+                },
+                email: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'email',
+                        placeholder: 'Your E-Mail'
+                    },
+                    value: '',
+                },
+                deliveryMethod: {
+                    elementType: 'select',
+                    elementConfig: {
+                        options: [
+                            {value: 'fastest', displayValue: 'Fastest'},
+                            {value: 'cheapest', displayValue: 'Cheapest'},
+                        ]
+                    },
+                    value: '',
+                },
+            },
+            loading: false
     }
 
     orderHandler = (event) => {
@@ -22,17 +68,7 @@ class ContactData extends Component {
         this.setState({ loading: true })
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'mutasem kwaik',
-                address: {
-                    street: 'alamal 1',
-                    zipCode: '2564',
-                    country: 'gaza'
-                },
-                email: 'motasemimad99@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         }
         axios.post('/orders.json', order)
         .then(response => {
@@ -47,7 +83,7 @@ class ContactData extends Component {
     render() {
         let form = (
                 <form>
-                    <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                    <Input elementType="..." elementConfig="..." value="..." />
                     <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
                     <Input inputtype="input" type="text" name="street" placeholder="Street" />
                     <Input inputtype="input" type="text" name="name" placeholder="Postal Code" />
