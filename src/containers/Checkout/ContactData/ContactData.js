059beb0db5b0e44fa1dd3383cb44b7ca14c4f44e
mutasem <motasemimad10@gmail.com>
2021-01-17 16:24:45 +0200

    learn react burger-app module13 lec8

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -80,13 +80,37 @@ class ContactData extends Component {
         })
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+        updatedFormElement.value = event.target.value
+        updatedOrderForm[inputIdentifier] = updatedFormElement
+        this.setState({ orderForm: updatedOrderForm })
+    }
+
     render() {
+        const formElementArray = []
+        for(let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
         let form = (
                 <form>
-                    <Input elementType="..." elementConfig="..." value="..." />
-                    <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-                    <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                    <Input inputtype="input" type="text" name="name" placeholder="Postal Code" />
+                    {formElementArray.map(formElement => (
+                        <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} 
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                    ))}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
         )
